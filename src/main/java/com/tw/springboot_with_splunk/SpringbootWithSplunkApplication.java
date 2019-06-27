03caba84f74b4d7989561f51b019561304319df7
aditilahare <aditilahare525@gmail.com>
2019-06-27 23:21:48 +0530

    [Aditi] : Initial setup.

---
+++ src/main/java/com/tw/springboot_with_splunk/SpringbootWithSplunkApplication.java
@@ -0,0 +1,13 @@
+package com.tw.springboot_with_splunk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithSplunkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithSplunkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tw/springboot_with_splunk/SpringbootWithSplunkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tw.springboot_with_splunk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootWithSplunkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
